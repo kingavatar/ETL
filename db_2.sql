2dc504bbe5132b8854de157aa606788592d9cbec
sanjay037 <sanjaykumar.reddy@iiitb.org>
2021-04-16 11:08:21 +0530

    Fixed ETL Engine

--- db_2.sql
+++ db_2.sql
@@ -5,9 +5,17 @@ create table employee(id int,salary float,location varchar(255));
 insert into employee values(1,100,"Bengaluru");
 insert into employee values(2,200,"Bezawada");
 insert into employee values(3,300,"Chennai");
+create table manager(id int,super_id int,name varchar(255));
+insert into manager values(1,1,"m1");
+insert into manager values(2,2,"m2");
+insert into manager values(3,3,"m3");
+create table query(id int);
+
+
 
 drop database datawarehouse;
 use datawarehouse;
 create table datawarehouse(id_1 int,salary_1 float,location_1 varchar(255));
+create table datawarehouse1(id_1 int,salary_1 float,location_1 varchar(255),name_1 varchar(255));
 
 
