a2819054f7b80b634111be5e85757df32790c01d
kingavatar <saikiranreddy06@gmail.com>
2021-04-16 12:17:48 +0530

    Updated files from backend

---
+++ backend/dist/precache-manifest.f794681001e9ba76b44d77377d7e215d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fb36c466ec72b3b6cb",
+    "url": "/css/app.74cafe46.css"
+  },
+  {
+    "revision": "91a16a4524457c4a0893",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "55c8d57289e648d900bec770bff1b382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e93bad81e01a2f4550b8",
+    "url": "/js/about.39d35ee4.js"
+  },
+  {
+    "revision": "e5fb36c466ec72b3b6cb",
+    "url": "/js/app.4c01384b.js"
+  },
+  {
+    "revision": "91a16a4524457c4a0893",
+    "url": "/js/chunk-vendors.e57c0973.js"
+  },
+  {
+    "revision": "b8df3ff4c6ab86099f273878a628e1f4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
